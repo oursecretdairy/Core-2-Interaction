63f5159557a3ac48d905148b518638fc00c57906
jinnkim <kimh000@newschool.edu>
2025-02-27 10:24:24 -0500

    change

---
+++ lightswitch/scripts.js
@@ -0,0 +1,29 @@
+// get the button elements
+var onButton = document.querySelector('button[name="on"]');
+var offButton = document.querySelector('button[name="off"]');
+
+// log the elements to the console
+console.log(onButton);
+console.log(offButton);
+
+// get the lightbulb element
+var lightbulb = document.querySelector('.lightbulb');
+
+// log the element to the lightbulb
+console.log(lightbulb);
+
+// add event listener to on button
+onButton.addEventListener('click', () => {
+
+    // add the is-on class to the lightbulb element
+    lightbulb.classList.add('is-on');
+
+});
+
+// add event listener to off button
+offButton.addEventListener('click', () => {
+
+    //remove the is-on class from the lightbulb element
+    lightbulb.classList.remove('is-on');
+
+});
