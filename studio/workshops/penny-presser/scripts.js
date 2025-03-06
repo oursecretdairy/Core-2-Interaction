7e5bbddf3e6ad8e330fb2669117395cf41ea7605
jinnkim <kimh000@newschool.edu>
2025-03-06 09:59:29 -0500

    change

--- studio/workshops/penny-presser/scripts.js
+++ studio/workshops/penny-presser/scripts.js
@@ -1,5 +1,6 @@
 // get the press button
 var pressButton = document.querySelector('button[name=press]');
+var studentSelect = document.querySelector('select[name="student"]');
 var pennySelect = document.querySelector('select[name=penny]');
 var pennyImage = document.querySelector('.penny');
 
@@ -10,12 +11,15 @@ console.log(pressButton);
 pressButton.addEventListener('click', () => {
 
 
+   // long the student vavlue
+   console.log(studentSelect.value)
+
    // log the penny value
     console.log(pennySelect.value);
 
     // set the penny image src
-    pennyImage.src = 'img/' + pennySelect.value;
-
+   // pennyImage.src = 'img/' + pennySelect.value;
+   pennyImage.src = 'https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/pennt-presser/img/' + pennySelect.value;
      //log the penny image
      console.log(pennyImage);
 })
